e911f3f8a38ad5a8e26e6623d2598abfa1818252
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-19 20:41:04 +0700

    Update lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[][M]){
+     
+     cout << "------------------------------------------------------------------------\n";
+     for(int row = 0; row < 30; row++){
+          cout << "|";
+          for(int column = 0; column < 70; column++){
+               if(image[row][column]){
+                    cout << "*";
+               }else{
+                    cout << " ";
+               }
+          }
+          cout << "|\n";
+     }
+     cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+     for(int row = 0; row < 30; row++){
+          for(int column = 0; column < 70; column++){
+               if(sqrt(pow(row - x,2)+pow(column - y,2)) <= s - 1){
+                    image[row][column] = 1;
+               }
+          }
+     }
+}
